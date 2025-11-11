4bfaebfc79f9959643ce9cad6be4ac211ad5a23b
Adi <adiqwertyuiop.09@gmail.com>
2025-11-12 07:39:16 +0800

    added UserDetails usercontrol

---
+++ SwineSyncc/Navigation/User Management/UserDetails.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SwineSyncc.Navigation.User_Management
+{
+    partial class UserDetails
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labellabel = new System.Windows.Forms.Label();
+            this.detailsuser = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.Label();
+            this.userDetailsBackBtn = new RoundedButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.userDetailsBackBtn);
+            this.panel1.Controls.Add(this.lblRole);
+            this.panel1.Controls.Add(this.lblPassword);
+            this.panel1.Controls.Add(this.lblUsername);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.labellabel);
+            this.panel1.Controls.Add(this.detailsuser);
+            this.panel1.Controls.Add(this.user);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1117, 1081);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRole.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.lblRole.Location = new System.Drawing.Point(353, 302);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(69, 41);
+            this.lblRole.TabIndex = 44;
+            this.lblRole.Text = "role";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.lblPassword.Location = new System.Drawing.Point(760, 164);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(145, 41);
+            this.lblPassword.TabIndex = 43;
+            this.lblPassword.Text = "password";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.lblUsername.Location = new System.Drawing.Point(353, 165);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(148, 41);
+            this.lblUsername.TabIndex = 42;
+            this.lblUsername.Text = "username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label2.Location = new System.Drawing.Point(133, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 41);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Role:";
+            // 
+            // labellabel
+            // 
+            this.labellabel.AutoSize = true;
+            this.labellabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labellabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.labellabel.Location = new System.Drawing.Point(540, 165);
+            this.labellabel.Name = "labellabel";
+            this.labellabel.Size = new System.Drawing.Size(158, 41);
+            this.labellabel.TabIndex = 40;
+            this.labellabel.Text = "Password:";
+            // 
+            // detailsuser
+            // 
+            this.detailsuser.AutoSize = true;
+            this.detailsuser.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detailsuser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.detailsuser.Location = new System.Drawing.Point(46, 55);
+            this.detailsuser.Name = "detailsuser";
+            this.detailsuser.Size = new System.Drawing.Size(244, 54);
+            this.detailsuser.TabIndex = 38;
+            this.detailsuser.Text = "User details";
+            // 
+            // user
+            // 
+            this.user.AutoSize = true;
+            this.user.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.user.Location = new System.Drawing.Point(133, 165);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(166, 41);
+            this.user.TabIndex = 39;
+            this.user.Text = "Username:";
+            // 
+            // userDetailsBackBtn
+            // 
+            this.userDetailsBackBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.userDetailsBackBtn.BorderRadious = 9;
+            this.userDetailsBackBtn.FlatAppearance.BorderSize = 0;
+            this.userDetailsBackBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.userDetailsBackBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userDetailsBackBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.userDetailsBackBtn.Location = new System.Drawing.Point(689, 493);
+            this.userDetailsBackBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.userDetailsBackBtn.Name = "userDetailsBackBtn";
+            this.userDetailsBackBtn.Size = new System.Drawing.Size(213, 79);
+            this.userDetailsBackBtn.TabIndex = 69;
+            this.userDetailsBackBtn.Text = "< Back";
+            this.userDetailsBackBtn.UseVisualStyleBackColor = false;
+            this.userDetailsBackBtn.Click += new System.EventHandler(this.userDetailsBackBtn_Click);
+            // 
+            // UserDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "UserDetails";
+            this.Size = new System.Drawing.Size(1117, 1081);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labellabel;
+        private System.Windows.Forms.Label detailsuser;
+        private System.Windows.Forms.Label user;
+        private RoundedButton userDetailsBackBtn;
+    }
+}
