f3145b354bcc166fbf2a5dc1ed2fbd6a61257db1
Schtemberg0147 <joseiiis013@gmail.com>
2025-11-11 21:16:15 +0800

    Added Gradient_RoundedFlowLayoutPanel

---
+++ SwineSyncc/CustomUIElements/Gradient_RoundedFlowLayoutPanel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwineSyncc.CustomUIElements
+{
+    internal class Gradient_RoundedFlowLayoutPanel: FlowLayoutPanel
+    {
+        //Fields
+        private int borderRadius = 30;
+        private float gradientAngle = 90F;
+        private Color gradientTopColor = Color.DodgerBlue;
+        private Color gradientBottomColor = Color.CadetBlue;
+
+        //Constructor
+        public Gradient_RoundedFlowLayoutPanel()
+        {
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+            this.Size = new Size(350, 200);
+        }
+
+        //Properties
+        [Category("Appearance")]
+        [Description("Sets the corner radius of the panel.")]
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public int BorderRadius
+        {
+            get => borderRadius;
+            set { borderRadius = value; this.Invalidate(); }
+        }
+
+        [Category("Appearance")]
+        [Description("Sets the gradient angle of the panel.")]
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public float GradientAngle
+        {
+            get => gradientAngle;
+            set { gradientAngle = value; this.Invalidate(); }
+        }
+
+        [Category("Appearance")]
+        [Description("Sets the top color of the panel.")]
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public Color GradientTopColor
+        {
+            get => gradientTopColor;
+            set { gradientTopColor = value; this.Invalidate(); }
+        }
+
+        [Category("Appearance")]
+        [Description("Sets the bottom color of the panel.")]
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public Color GradientBottomColor
+        {
+            get => gradientBottomColor;
+            set { gradientBottomColor = value; this.Invalidate(); }
+        }
+
+        //Methods
+        private GraphicsPath GetArtanPath(RectangleF rectangle, float radius)
+        {
+            GraphicsPath graphicsPath = new GraphicsPath();
+            graphicsPath.StartFigure();
+            graphicsPath.AddArc(rectangle.Width - radius, rectangle.Height - radius, radius, radius, 0, 90);
+            graphicsPath.AddArc(rectangle.X, rectangle.Height - radius, radius, radius, 90, 90);
+            graphicsPath.AddArc(rectangle.X, rectangle.Y, radius, radius, 180, 90);
+            graphicsPath.AddArc(rectangle.Width - radius, rectangle.Y, radius, radius, 270, 90);
+            graphicsPath.CloseFigure();
+            return graphicsPath;
+        }
+
+        //Overriden Methods
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            //Gradient
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            LinearGradientBrush brushArtan = new LinearGradientBrush(this.ClientRectangle, this.GradientTopColor, this.gradientBottomColor, this.gradientAngle);
+            Graphics graphicsArtan = e.Graphics;
+            graphicsArtan.FillRectangle(brushArtan, ClientRectangle);
+
+            //Border Radius
+            RectangleF rectangleF = new RectangleF(0, 0, this.Width, this.Height);
+            if (borderRadius > 2)
+            {
+                using (GraphicsPath graphicsPath = GetArtanPath(rectangleF, borderRadius))
+                using (Pen pen = new Pen(this.Parent.BackColor, 2))
+                {
+                    this.Region = new Region(graphicsPath);
+                    e.Graphics.DrawPath(pen, graphicsPath);
+                }
+            }
+            else
+            {
+                this.Region = new Region(rectangleF);
+            }
+        }
+    }
+}
