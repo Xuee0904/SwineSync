144bcfecedf8dc9ff37c075911e2ee4c264c4d95
Xuee0904 <cedric2827@gmail.com>
2025-11-27 21:11:16 +0800

    Added a method to papulate the combo box(AddPregnancy)

--- SwineSyncc/Navigation/AddPregnancy.cs
+++ SwineSyncc/Navigation/AddPregnancy.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SwineSyncc.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,54 @@ namespace SwineSyncc.Navigation
         public AddPregnancy()
         {
             InitializeComponent();
+
+            LoadComboBreedingID();
+            LoadComboPregnantSow();
         }
+
+        private void LoadComboBreedingID()
+        {
+            string query = "SELECT BreedingID FROM BreedingRecords WHERE Result = 'Success' ORDER BY BreedingID";
+
+            using (SqlConnection conn = DBConnection.Instance.GetConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        comboBreedingID.Items.Clear();
+                        while (reader.Read())
+                        {
+                            comboBreedingID.Items.Add(reader["BreedingID"].ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void LoadComboPregnantSow()
+        {
+            string query = "SELECT P.Name, B.BreedingID FROM Pigs AS P INNER JOIN BreedingRecords AS B ON P.PigID = B.SowID WHERE B.Result = 'Success';";
+
+            using (SqlConnection conn = DBConnection.Instance.GetConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        comboPregnantSow.Items.Clear();
+                        while (reader.Read())
+                        {
+                            comboPregnantSow.Items.Add(reader["Name"].ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+
+
     }
 }
