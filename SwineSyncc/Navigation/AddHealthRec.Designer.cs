af9c4c0d15ceb8abddd150a655390eb9f2b4db3c
Adi <adiqwertyuiop.09@gmail.com>
2025-11-29 18:34:12 +0800

    added AddHealthRec user control

---
+++ SwineSyncc/Navigation/AddHealthRec.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace SwineSyncc.Navigation
+{
+    partial class AddHealthRec
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statuslbl = new System.Windows.Forms.Label();
+            this.treatmentTxt = new System.Windows.Forms.TextBox();
+            this.weightlbl = new System.Windows.Forms.Label();
+            this.dtCheckUp = new System.Windows.Forms.DateTimePicker();
+            this.checkuplabel = new System.Windows.Forms.Label();
+            this.breedlbl = new System.Windows.Forms.Label();
+            this.pignamelabel = new System.Windows.Forms.Label();
+            this.healthrecadd = new System.Windows.Forms.Label();
+            this.comboHealthPigName = new System.Windows.Forms.ComboBox();
+            this.conditionTxt = new System.Windows.Forms.TextBox();
+            this.vetNameTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notesTxt = new System.Windows.Forms.TextBox();
+            this.cancelbtn = new BorderRoundedButton();
+            this.clearbtn = new BorderRoundedButton();
+            this.savebtn = new RoundedButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cancelbtn);
+            this.panel1.Controls.Add(this.clearbtn);
+            this.panel1.Controls.Add(this.savebtn);
+            this.panel1.Controls.Add(this.notesTxt);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.vetNameTxt);
+            this.panel1.Controls.Add(this.conditionTxt);
+            this.panel1.Controls.Add(this.comboHealthPigName);
+            this.panel1.Controls.Add(this.healthrecadd);
+            this.panel1.Controls.Add(this.statuslbl);
+            this.panel1.Controls.Add(this.treatmentTxt);
+            this.panel1.Controls.Add(this.weightlbl);
+            this.panel1.Controls.Add(this.dtCheckUp);
+            this.panel1.Controls.Add(this.checkuplabel);
+            this.panel1.Controls.Add(this.breedlbl);
+            this.panel1.Controls.Add(this.pignamelabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1200, 645);
+            this.panel1.TabIndex = 0;
+            // 
+            // statuslbl
+            // 
+            this.statuslbl.AutoSize = true;
+            this.statuslbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statuslbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.statuslbl.Location = new System.Drawing.Point(58, 433);
+            this.statuslbl.Name = "statuslbl";
+            this.statuslbl.Size = new System.Drawing.Size(157, 41);
+            this.statuslbl.TabIndex = 54;
+            this.statuslbl.Text = "Vet name:";
+            // 
+            // treatmentTxt
+            // 
+            this.treatmentTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treatmentTxt.Location = new System.Drawing.Point(619, 339);
+            this.treatmentTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.treatmentTxt.Name = "treatmentTxt";
+            this.treatmentTxt.Size = new System.Drawing.Size(517, 37);
+            this.treatmentTxt.TabIndex = 51;
+            // 
+            // weightlbl
+            // 
+            this.weightlbl.AutoSize = true;
+            this.weightlbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightlbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.weightlbl.Location = new System.Drawing.Point(606, 296);
+            this.weightlbl.Name = "weightlbl";
+            this.weightlbl.Size = new System.Drawing.Size(163, 41);
+            this.weightlbl.TabIndex = 50;
+            this.weightlbl.Text = "Treatment";
+            // 
+            // dtCheckUp
+            // 
+            this.dtCheckUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtCheckUp.Location = new System.Drawing.Point(619, 221);
+            this.dtCheckUp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtCheckUp.Name = "dtCheckUp";
+            this.dtCheckUp.Size = new System.Drawing.Size(517, 37);
+            this.dtCheckUp.TabIndex = 49;
+            // 
+            // checkuplabel
+            // 
+            this.checkuplabel.AutoSize = true;
+            this.checkuplabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkuplabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.checkuplabel.Location = new System.Drawing.Point(607, 182);
+            this.checkuplabel.Name = "checkuplabel";
+            this.checkuplabel.Size = new System.Drawing.Size(228, 41);
+            this.checkuplabel.TabIndex = 48;
+            this.checkuplabel.Text = "Check-up date:";
+            // 
+            // breedlbl
+            // 
+            this.breedlbl.AutoSize = true;
+            this.breedlbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.breedlbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.breedlbl.Location = new System.Drawing.Point(58, 296);
+            this.breedlbl.Name = "breedlbl";
+            this.breedlbl.Size = new System.Drawing.Size(158, 41);
+            this.breedlbl.TabIndex = 47;
+            this.breedlbl.Text = "Condition";
+            // 
+            // pignamelabel
+            // 
+            this.pignamelabel.AutoSize = true;
+            this.pignamelabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pignamelabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.pignamelabel.Location = new System.Drawing.Point(55, 182);
+            this.pignamelabel.Name = "pignamelabel";
+            this.pignamelabel.Size = new System.Drawing.Size(157, 41);
+            this.pignamelabel.TabIndex = 45;
+            this.pignamelabel.Text = "Pig name:";
+            // 
+            // healthrecadd
+            // 
+            this.healthrecadd.AutoSize = true;
+            this.healthrecadd.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthrecadd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.healthrecadd.Location = new System.Drawing.Point(56, 50);
+            this.healthrecadd.Name = "healthrecadd";
+            this.healthrecadd.Size = new System.Drawing.Size(222, 54);
+            this.healthrecadd.TabIndex = 55;
+            this.healthrecadd.Text = "Add piglet";
+            // 
+            // comboHealthPigName
+            // 
+            this.comboHealthPigName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboHealthPigName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboHealthPigName.FormattingEnabled = true;
+            this.comboHealthPigName.Items.AddRange(new object[] {
+            "Berkshire",
+            "Chester White",
+            "Duroc",
+            "Hampshire",
+            "Hereford",
+            "Kunekune",
+            "Landrace",
+            "Large White",
+            "Mangalista",
+            "Pietrain",
+            "Pot-bellied Pig",
+            "Tamworth",
+            "Yorkshire"});
+            this.comboHealthPigName.Location = new System.Drawing.Point(66, 223);
+            this.comboHealthPigName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboHealthPigName.Name = "comboHealthPigName";
+            this.comboHealthPigName.Size = new System.Drawing.Size(517, 38);
+            this.comboHealthPigName.TabIndex = 56;
+            // 
+            // conditionTxt
+            // 
+            this.conditionTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conditionTxt.Location = new System.Drawing.Point(66, 339);
+            this.conditionTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.conditionTxt.Name = "conditionTxt";
+            this.conditionTxt.Size = new System.Drawing.Size(517, 37);
+            this.conditionTxt.TabIndex = 57;
+            // 
+            // vetNameTxt
+            // 
+            this.vetNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vetNameTxt.Location = new System.Drawing.Point(66, 476);
+            this.vetNameTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.vetNameTxt.Name = "vetNameTxt";
+            this.vetNameTxt.Size = new System.Drawing.Size(517, 37);
+            this.vetNameTxt.TabIndex = 58;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label1.Location = new System.Drawing.Point(612, 433);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 41);
+            this.label1.TabIndex = 59;
+            this.label1.Text = "Notes:";
+            // 
+            // notesTxt
+            // 
+            this.notesTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notesTxt.Location = new System.Drawing.Point(619, 476);
+            this.notesTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.notesTxt.Name = "notesTxt";
+            this.notesTxt.Size = new System.Drawing.Size(517, 37);
+            this.notesTxt.TabIndex = 60;
+            // 
+            // cancelbtn
+            // 
+            this.cancelbtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.cancelbtn.BorderRadious = 9;
+            this.cancelbtn.BorderThickness = 3;
+            this.cancelbtn.FlatAppearance.BorderSize = 0;
+            this.cancelbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelbtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.cancelbtn.Location = new System.Drawing.Point(794, 631);
+            this.cancelbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelbtn.Name = "cancelbtn";
+            this.cancelbtn.Size = new System.Drawing.Size(168, 55);
+            this.cancelbtn.TabIndex = 63;
+            this.cancelbtn.Text = "Cancel";
+            this.cancelbtn.UseVisualStyleBackColor = true;
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.clearbtn.BorderRadious = 9;
+            this.clearbtn.BorderThickness = 3;
+            this.clearbtn.FlatAppearance.BorderSize = 0;
+            this.clearbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearbtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.clearbtn.Location = new System.Drawing.Point(619, 631);
+            this.clearbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(168, 55);
+            this.clearbtn.TabIndex = 62;
+            this.clearbtn.Text = "Clear";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            // 
+            // savebtn
+            // 
+            this.savebtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.savebtn.BorderRadious = 9;
+            this.savebtn.FlatAppearance.BorderSize = 0;
+            this.savebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.savebtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.savebtn.Location = new System.Drawing.Point(968, 631);
+            this.savebtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(168, 55);
+            this.savebtn.TabIndex = 61;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = false;
+            // 
+            // AddHealthRec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "AddHealthRec";
+            this.Size = new System.Drawing.Size(1200, 645);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label statuslbl;
+        private System.Windows.Forms.TextBox treatmentTxt;
+        private System.Windows.Forms.Label weightlbl;
+        private System.Windows.Forms.DateTimePicker dtCheckUp;
+        private System.Windows.Forms.Label checkuplabel;
+        private System.Windows.Forms.Label breedlbl;
+        private System.Windows.Forms.Label pignamelabel;
+        private System.Windows.Forms.Label healthrecadd;
+        private System.Windows.Forms.ComboBox comboHealthPigName;
+        private System.Windows.Forms.TextBox notesTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox vetNameTxt;
+        private System.Windows.Forms.TextBox conditionTxt;
+        private BorderRoundedButton cancelbtn;
+        private BorderRoundedButton clearbtn;
+        private RoundedButton savebtn;
+    }
+}
