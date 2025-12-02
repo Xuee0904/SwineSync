74a8e6255d755b48ec8fc4fa91781b47ae2e6a8d
Adi <adiqwertyuiop.09@gmail.com>
2025-12-02 17:17:14 +0800

    created AddItem user control

---
+++ SwineSyncc/Navigation/AddItem.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace SwineSyncc.Navigation
+{
+    partial class AddItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.additemlabel = new System.Windows.Forms.Label();
+            this.dtRecordDate = new System.Windows.Forms.DateTimePicker();
+            this.statuslbl = new System.Windows.Forms.Label();
+            this.comboProduct = new System.Windows.Forms.ComboBox();
+            this.quantitylabel = new System.Windows.Forms.Label();
+            this.productlabel = new System.Windows.Forms.Label();
+            this.quantityTxt = new System.Windows.Forms.TextBox();
+            this.dtExpirationDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.descriptionTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cancelbtn = new BorderRoundedButton();
+            this.clearbtn = new BorderRoundedButton();
+            this.savebtn = new RoundedButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cancelbtn);
+            this.panel1.Controls.Add(this.clearbtn);
+            this.panel1.Controls.Add(this.savebtn);
+            this.panel1.Controls.Add(this.descriptionTxt);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dtExpirationDate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.quantityTxt);
+            this.panel1.Controls.Add(this.dtRecordDate);
+            this.panel1.Controls.Add(this.statuslbl);
+            this.panel1.Controls.Add(this.comboProduct);
+            this.panel1.Controls.Add(this.quantitylabel);
+            this.panel1.Controls.Add(this.productlabel);
+            this.panel1.Controls.Add(this.additemlabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1200, 645);
+            this.panel1.TabIndex = 0;
+            // 
+            // additemlabel
+            // 
+            this.additemlabel.AutoSize = true;
+            this.additemlabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.additemlabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.additemlabel.Location = new System.Drawing.Point(56, 50);
+            this.additemlabel.Name = "additemlabel";
+            this.additemlabel.Size = new System.Drawing.Size(198, 54);
+            this.additemlabel.TabIndex = 62;
+            this.additemlabel.Text = "Add item";
+            // 
+            // dtRecordDate
+            // 
+            this.dtRecordDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtRecordDate.Location = new System.Drawing.Point(66, 350);
+            this.dtRecordDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtRecordDate.Name = "dtRecordDate";
+            this.dtRecordDate.Size = new System.Drawing.Size(517, 37);
+            this.dtRecordDate.TabIndex = 68;
+            // 
+            // statuslbl
+            // 
+            this.statuslbl.AutoSize = true;
+            this.statuslbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statuslbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.statuslbl.Location = new System.Drawing.Point(58, 308);
+            this.statuslbl.Name = "statuslbl";
+            this.statuslbl.Size = new System.Drawing.Size(195, 41);
+            this.statuslbl.TabIndex = 67;
+            this.statuslbl.Text = "Record date:";
+            // 
+            // comboProduct
+            // 
+            this.comboProduct.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboProduct.FormattingEnabled = true;
+            this.comboProduct.Items.AddRange(new object[] {
+            "Alive",
+            "For Breeding",
+            "Weaned",
+            "Sold",
+            "Slaughtered",
+            "Deceased",
+            "Sick",
+            "Quarantined"});
+            this.comboProduct.Location = new System.Drawing.Point(62, 225);
+            this.comboProduct.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboProduct.Name = "comboProduct";
+            this.comboProduct.Size = new System.Drawing.Size(517, 38);
+            this.comboProduct.TabIndex = 66;
+            // 
+            // quantitylabel
+            // 
+            this.quantitylabel.AutoSize = true;
+            this.quantitylabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantitylabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.quantitylabel.Location = new System.Drawing.Point(645, 182);
+            this.quantitylabel.Name = "quantitylabel";
+            this.quantitylabel.Size = new System.Drawing.Size(150, 41);
+            this.quantitylabel.TabIndex = 64;
+            this.quantitylabel.Text = "Quantity:";
+            // 
+            // productlabel
+            // 
+            this.productlabel.AutoSize = true;
+            this.productlabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productlabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.productlabel.Location = new System.Drawing.Point(55, 182);
+            this.productlabel.Name = "productlabel";
+            this.productlabel.Size = new System.Drawing.Size(208, 41);
+            this.productlabel.TabIndex = 63;
+            this.productlabel.Text = "Product type:";
+            // 
+            // quantityTxt
+            // 
+            this.quantityTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityTxt.Location = new System.Drawing.Point(652, 225);
+            this.quantityTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.quantityTxt.Name = "quantityTxt";
+            this.quantityTxt.Size = new System.Drawing.Size(517, 37);
+            this.quantityTxt.TabIndex = 71;
+            // 
+            // dtExpirationDate
+            // 
+            this.dtExpirationDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtExpirationDate.Location = new System.Drawing.Point(652, 350);
+            this.dtExpirationDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtExpirationDate.Name = "dtExpirationDate";
+            this.dtExpirationDate.Size = new System.Drawing.Size(517, 37);
+            this.dtExpirationDate.TabIndex = 73;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label1.Location = new System.Drawing.Point(644, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 41);
+            this.label1.TabIndex = 72;
+            this.label1.Text = "Expiration date:";
+            // 
+            // descriptionTxt
+            // 
+            this.descriptionTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionTxt.Location = new System.Drawing.Point(66, 482);
+            this.descriptionTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.descriptionTxt.Name = "descriptionTxt";
+            this.descriptionTxt.Size = new System.Drawing.Size(517, 37);
+            this.descriptionTxt.TabIndex = 75;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label2.Location = new System.Drawing.Point(59, 439);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 41);
+            this.label2.TabIndex = 74;
+            this.label2.Text = "Description:";
+            // 
+            // cancelbtn
+            // 
+            this.cancelbtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.cancelbtn.BorderRadious = 9;
+            this.cancelbtn.BorderThickness = 3;
+            this.cancelbtn.FlatAppearance.BorderSize = 0;
+            this.cancelbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelbtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.cancelbtn.Location = new System.Drawing.Point(793, 631);
+            this.cancelbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelbtn.Name = "cancelbtn";
+            this.cancelbtn.Size = new System.Drawing.Size(168, 55);
+            this.cancelbtn.TabIndex = 78;
+            this.cancelbtn.Text = "Cancel";
+            this.cancelbtn.UseVisualStyleBackColor = true;
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.clearbtn.BorderRadious = 9;
+            this.clearbtn.BorderThickness = 3;
+            this.clearbtn.FlatAppearance.BorderSize = 0;
+            this.clearbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearbtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.clearbtn.Location = new System.Drawing.Point(617, 631);
+            this.clearbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(168, 55);
+            this.clearbtn.TabIndex = 77;
+            this.clearbtn.Text = "Clear";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            // 
+            // savebtn
+            // 
+            this.savebtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.savebtn.BorderRadious = 9;
+            this.savebtn.FlatAppearance.BorderSize = 0;
+            this.savebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.savebtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.savebtn.Location = new System.Drawing.Point(968, 631);
+            this.savebtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(168, 55);
+            this.savebtn.TabIndex = 76;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = false;
+            // 
+            // AddItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "AddItem";
+            this.Size = new System.Drawing.Size(1200, 645);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label additemlabel;
+        private System.Windows.Forms.DateTimePicker dtRecordDate;
+        private System.Windows.Forms.Label statuslbl;
+        private System.Windows.Forms.ComboBox comboProduct;
+        private System.Windows.Forms.Label quantitylabel;
+        private System.Windows.Forms.Label productlabel;
+        private System.Windows.Forms.TextBox quantityTxt;
+        private System.Windows.Forms.TextBox descriptionTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtExpirationDate;
+        private System.Windows.Forms.Label label1;
+        private BorderRoundedButton cancelbtn;
+        private BorderRoundedButton clearbtn;
+        private RoundedButton savebtn;
+    }
+}
