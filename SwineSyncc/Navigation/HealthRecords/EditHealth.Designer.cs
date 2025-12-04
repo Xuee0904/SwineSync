4de38f03fb80fb99e85abbeff1f53b8f12b5e7bd
Adi <adiqwertyuiop.09@gmail.com>
2025-12-05 02:55:36 +0800

    added EditHealth and EditPregnancy user control

---
+++ SwineSyncc/Navigation/HealthRecords/EditHealth.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace SwineSyncc.Navigation.HealthRecords
+{
+    partial class EditHealth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EditNotesTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EditVetNameTxt = new System.Windows.Forms.TextBox();
+            this.EditConditionTxt = new System.Windows.Forms.TextBox();
+            this.EditComboHealthPigName = new System.Windows.Forms.ComboBox();
+            this.healthrecadd = new System.Windows.Forms.Label();
+            this.statuslbl = new System.Windows.Forms.Label();
+            this.EditTreatmentTxt = new System.Windows.Forms.TextBox();
+            this.weightlbl = new System.Windows.Forms.Label();
+            this.EditDtCheckUp = new System.Windows.Forms.DateTimePicker();
+            this.checkuplabel = new System.Windows.Forms.Label();
+            this.breedlbl = new System.Windows.Forms.Label();
+            this.pignamelabel = new System.Windows.Forms.Label();
+            this.buttonGroup1 = new SwineSyncc.CustomUIElements.ButtonGroup.ButtonGroup();
+            this.SuspendLayout();
+            // 
+            // EditNotesTxt
+            // 
+            this.EditNotesTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditNotesTxt.Location = new System.Drawing.Point(497, 412);
+            this.EditNotesTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditNotesTxt.Name = "EditNotesTxt";
+            this.EditNotesTxt.Size = new System.Drawing.Size(394, 37);
+            this.EditNotesTxt.TabIndex = 73;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label1.Location = new System.Drawing.Point(490, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 41);
+            this.label1.TabIndex = 72;
+            this.label1.Text = "Notes:";
+            // 
+            // EditVetNameTxt
+            // 
+            this.EditVetNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditVetNameTxt.Location = new System.Drawing.Point(50, 412);
+            this.EditVetNameTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditVetNameTxt.Name = "EditVetNameTxt";
+            this.EditVetNameTxt.Size = new System.Drawing.Size(394, 37);
+            this.EditVetNameTxt.TabIndex = 71;
+            // 
+            // EditConditionTxt
+            // 
+            this.EditConditionTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditConditionTxt.Location = new System.Drawing.Point(50, 274);
+            this.EditConditionTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditConditionTxt.Name = "EditConditionTxt";
+            this.EditConditionTxt.Size = new System.Drawing.Size(394, 37);
+            this.EditConditionTxt.TabIndex = 70;
+            // 
+            // EditComboHealthPigName
+            // 
+            this.EditComboHealthPigName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EditComboHealthPigName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditComboHealthPigName.FormattingEnabled = true;
+            this.EditComboHealthPigName.Items.AddRange(new object[] {
+            "Berkshire",
+            "Chester White",
+            "Duroc",
+            "Hampshire",
+            "Hereford",
+            "Kunekune",
+            "Landrace",
+            "Large White",
+            "Mangalista",
+            "Pietrain",
+            "Pot-bellied Pig",
+            "Tamworth",
+            "Yorkshire"});
+            this.EditComboHealthPigName.Location = new System.Drawing.Point(50, 159);
+            this.EditComboHealthPigName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditComboHealthPigName.Name = "EditComboHealthPigName";
+            this.EditComboHealthPigName.Size = new System.Drawing.Size(394, 38);
+            this.EditComboHealthPigName.TabIndex = 69;
+            // 
+            // healthrecadd
+            // 
+            this.healthrecadd.AutoSize = true;
+            this.healthrecadd.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthrecadd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.healthrecadd.Location = new System.Drawing.Point(36, 30);
+            this.healthrecadd.Name = "healthrecadd";
+            this.healthrecadd.Size = new System.Drawing.Size(359, 54);
+            this.healthrecadd.TabIndex = 68;
+            this.healthrecadd.Text = "Edit health record";
+            // 
+            // statuslbl
+            // 
+            this.statuslbl.AutoSize = true;
+            this.statuslbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statuslbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.statuslbl.Location = new System.Drawing.Point(42, 369);
+            this.statuslbl.Name = "statuslbl";
+            this.statuslbl.Size = new System.Drawing.Size(283, 41);
+            this.statuslbl.TabIndex = 67;
+            this.statuslbl.Text = "Veterinarian name:";
+            // 
+            // EditTreatmentTxt
+            // 
+            this.EditTreatmentTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditTreatmentTxt.Location = new System.Drawing.Point(497, 274);
+            this.EditTreatmentTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditTreatmentTxt.Name = "EditTreatmentTxt";
+            this.EditTreatmentTxt.Size = new System.Drawing.Size(394, 37);
+            this.EditTreatmentTxt.TabIndex = 66;
+            // 
+            // weightlbl
+            // 
+            this.weightlbl.AutoSize = true;
+            this.weightlbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightlbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.weightlbl.Location = new System.Drawing.Point(483, 231);
+            this.weightlbl.Name = "weightlbl";
+            this.weightlbl.Size = new System.Drawing.Size(163, 41);
+            this.weightlbl.TabIndex = 65;
+            this.weightlbl.Text = "Treatment";
+            // 
+            // EditDtCheckUp
+            // 
+            this.EditDtCheckUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditDtCheckUp.Location = new System.Drawing.Point(497, 158);
+            this.EditDtCheckUp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditDtCheckUp.Name = "EditDtCheckUp";
+            this.EditDtCheckUp.Size = new System.Drawing.Size(468, 37);
+            this.EditDtCheckUp.TabIndex = 64;
+            // 
+            // checkuplabel
+            // 
+            this.checkuplabel.AutoSize = true;
+            this.checkuplabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkuplabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.checkuplabel.Location = new System.Drawing.Point(485, 118);
+            this.checkuplabel.Name = "checkuplabel";
+            this.checkuplabel.Size = new System.Drawing.Size(228, 41);
+            this.checkuplabel.TabIndex = 63;
+            this.checkuplabel.Text = "Check-up date:";
+            // 
+            // breedlbl
+            // 
+            this.breedlbl.AutoSize = true;
+            this.breedlbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.breedlbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.breedlbl.Location = new System.Drawing.Point(42, 231);
+            this.breedlbl.Name = "breedlbl";
+            this.breedlbl.Size = new System.Drawing.Size(158, 41);
+            this.breedlbl.TabIndex = 62;
+            this.breedlbl.Text = "Condition";
+            // 
+            // pignamelabel
+            // 
+            this.pignamelabel.AutoSize = true;
+            this.pignamelabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pignamelabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.pignamelabel.Location = new System.Drawing.Point(38, 118);
+            this.pignamelabel.Name = "pignamelabel";
+            this.pignamelabel.Size = new System.Drawing.Size(157, 41);
+            this.pignamelabel.TabIndex = 61;
+            this.pignamelabel.Text = "Pig name:";
+            // 
+            // buttonGroup1
+            // 
+            this.buttonGroup1.Location = new System.Drawing.Point(490, 485);
+            this.buttonGroup1.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonGroup1.Name = "buttonGroup1";
+            this.buttonGroup1.Size = new System.Drawing.Size(519, 55);
+            this.buttonGroup1.TabIndex = 74;
+            // 
+            // EditHealth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 588);
+            this.Controls.Add(this.buttonGroup1);
+            this.Controls.Add(this.EditNotesTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EditVetNameTxt);
+            this.Controls.Add(this.EditConditionTxt);
+            this.Controls.Add(this.EditComboHealthPigName);
+            this.Controls.Add(this.healthrecadd);
+            this.Controls.Add(this.statuslbl);
+            this.Controls.Add(this.EditTreatmentTxt);
+            this.Controls.Add(this.weightlbl);
+            this.Controls.Add(this.EditDtCheckUp);
+            this.Controls.Add(this.checkuplabel);
+            this.Controls.Add(this.breedlbl);
+            this.Controls.Add(this.pignamelabel);
+            this.Name = "EditHealth";
+            this.Text = "EditHealth";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EditNotesTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EditVetNameTxt;
+        private System.Windows.Forms.TextBox EditConditionTxt;
+        private System.Windows.Forms.ComboBox EditComboHealthPigName;
+        private System.Windows.Forms.Label healthrecadd;
+        private System.Windows.Forms.Label statuslbl;
+        private System.Windows.Forms.TextBox EditTreatmentTxt;
+        private System.Windows.Forms.Label weightlbl;
+        private System.Windows.Forms.DateTimePicker EditDtCheckUp;
+        private System.Windows.Forms.Label checkuplabel;
+        private System.Windows.Forms.Label breedlbl;
+        private System.Windows.Forms.Label pignamelabel;
+        private CustomUIElements.ButtonGroup.ButtonGroup buttonGroup1;
+    }
+}
---
+++ SwineSyncc/Navigation/PregnancyRecords/EditPregnancy.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace SwineSyncc.Navigation.PregnancyRecords
+{
+    partial class EditPregnancy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EditComboBreedingID = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EditDtConfirmation = new System.Windows.Forms.DateTimePicker();
+            this.EditDtExpected = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EditComboPregnantSow = new System.Windows.Forms.ComboBox();
+            this.pregnantlbl = new System.Windows.Forms.Label();
+            this.addbreedinglabel = new System.Windows.Forms.Label();
+            this.buttonGroup1 = new SwineSyncc.CustomUIElements.ButtonGroup.ButtonGroup();
+            this.SuspendLayout();
+            // 
+            // EditComboBreedingID
+            // 
+            this.EditComboBreedingID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EditComboBreedingID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditComboBreedingID.FormattingEnabled = true;
+            this.EditComboBreedingID.Items.AddRange(new object[] {
+            "Alive",
+            "For Breeding",
+            "Weaned",
+            "Sold",
+            "Slaughtered",
+            "Deceased",
+            "Sick",
+            "Quarantined"});
+            this.EditComboBreedingID.Location = new System.Drawing.Point(44, 199);
+            this.EditComboBreedingID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditComboBreedingID.Name = "EditComboBreedingID";
+            this.EditComboBreedingID.Size = new System.Drawing.Size(432, 38);
+            this.EditComboBreedingID.TabIndex = 85;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label3.Location = new System.Drawing.Point(38, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 41);
+            this.label3.TabIndex = 84;
+            this.label3.Text = "Breeding ID:";
+            // 
+            // EditDtConfirmation
+            // 
+            this.EditDtConfirmation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditDtConfirmation.Location = new System.Drawing.Point(517, 348);
+            this.EditDtConfirmation.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditDtConfirmation.Name = "EditDtConfirmation";
+            this.EditDtConfirmation.Size = new System.Drawing.Size(432, 37);
+            this.EditDtConfirmation.TabIndex = 83;
+            // 
+            // EditDtExpected
+            // 
+            this.EditDtExpected.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditDtExpected.Location = new System.Drawing.Point(44, 349);
+            this.EditDtExpected.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditDtExpected.Name = "EditDtExpected";
+            this.EditDtExpected.Size = new System.Drawing.Size(432, 37);
+            this.EditDtExpected.TabIndex = 82;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label2.Location = new System.Drawing.Point(511, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(395, 41);
+            this.label2.TabIndex = 81;
+            this.label2.Text = "Confirmed farrowing date:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.label1.Location = new System.Drawing.Point(38, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(374, 41);
+            this.label1.TabIndex = 80;
+            this.label1.Text = "Expected farrowing date:";
+            // 
+            // EditComboPregnantSow
+            // 
+            this.EditComboPregnantSow.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EditComboPregnantSow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditComboPregnantSow.FormattingEnabled = true;
+            this.EditComboPregnantSow.Items.AddRange(new object[] {
+            "Alive",
+            "For Breeding",
+            "Weaned",
+            "Sold",
+            "Slaughtered",
+            "Deceased",
+            "Sick",
+            "Quarantined"});
+            this.EditComboPregnantSow.Location = new System.Drawing.Point(518, 199);
+            this.EditComboPregnantSow.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditComboPregnantSow.Name = "EditComboPregnantSow";
+            this.EditComboPregnantSow.Size = new System.Drawing.Size(432, 38);
+            this.EditComboPregnantSow.TabIndex = 79;
+            // 
+            // pregnantlbl
+            // 
+            this.pregnantlbl.AutoSize = true;
+            this.pregnantlbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pregnantlbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.pregnantlbl.Location = new System.Drawing.Point(509, 156);
+            this.pregnantlbl.Name = "pregnantlbl";
+            this.pregnantlbl.Size = new System.Drawing.Size(303, 41);
+            this.pregnantlbl.TabIndex = 78;
+            this.pregnantlbl.Text = "Pregnant sow name:";
+            // 
+            // addbreedinglabel
+            // 
+            this.addbreedinglabel.AutoSize = true;
+            this.addbreedinglabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addbreedinglabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(72)))), ((int)(((byte)(60)))));
+            this.addbreedinglabel.Location = new System.Drawing.Point(36, 30);
+            this.addbreedinglabel.Name = "addbreedinglabel";
+            this.addbreedinglabel.Size = new System.Drawing.Size(306, 54);
+            this.addbreedinglabel.TabIndex = 77;
+            this.addbreedinglabel.Text = "Edit pregnancy";
+            // 
+            // buttonGroup1
+            // 
+            this.buttonGroup1.Location = new System.Drawing.Point(491, 464);
+            this.buttonGroup1.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonGroup1.Name = "buttonGroup1";
+            this.buttonGroup1.Size = new System.Drawing.Size(520, 55);
+            this.buttonGroup1.TabIndex = 86;
+            // 
+            // EditPregnancy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 588);
+            this.Controls.Add(this.buttonGroup1);
+            this.Controls.Add(this.EditComboBreedingID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EditDtConfirmation);
+            this.Controls.Add(this.EditDtExpected);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EditComboPregnantSow);
+            this.Controls.Add(this.pregnantlbl);
+            this.Controls.Add(this.addbreedinglabel);
+            this.Name = "EditPregnancy";
+            this.Text = "EditPregnancy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomUIElements.ButtonGroup.ButtonGroup buttonGroup1;
+        private System.Windows.Forms.ComboBox EditComboBreedingID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker EditDtConfirmation;
+        private System.Windows.Forms.DateTimePicker EditDtExpected;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox EditComboPregnantSow;
+        private System.Windows.Forms.Label pregnantlbl;
+        private System.Windows.Forms.Label addbreedinglabel;
+    }
+}
