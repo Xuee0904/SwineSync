6bf36ddc269455f2327a6d6914c2f8927bfbb2a7
Adi <adiqwertyuiop.09@gmail.com>
2025-12-05 22:03:04 +0800

    x button on Form1

--- SwineSyncc/Login/Form1.Designer.cs
+++ SwineSyncc/Login/Form1.Designer.cs
@@ -54,10 +54,9 @@ namespace SwineSyncc
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(67, 176);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(89, 217);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(217, 47);
+            this.label1.Size = new System.Drawing.Size(273, 60);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to";
             // 
@@ -67,10 +66,9 @@ namespace SwineSyncc
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(81, 434);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(108, 534);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(194, 47);
+            this.label2.Size = new System.Drawing.Size(237, 59);
             this.label2.TabIndex = 2;
             this.label2.Text = "SwineSync";
             // 
@@ -78,9 +76,10 @@ namespace SwineSyncc
             // 
             this.LoginLabel.AutoSize = true;
             this.LoginLabel.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoginLabel.Location = new System.Drawing.Point(378, 45);
+            this.LoginLabel.Location = new System.Drawing.Point(504, 55);
+            this.LoginLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LoginLabel.Name = "LoginLabel";
-            this.LoginLabel.Size = new System.Drawing.Size(128, 47);
+            this.LoginLabel.Size = new System.Drawing.Size(160, 60);
             this.LoginLabel.TabIndex = 10;
             this.LoginLabel.Text = "LOGIN";
             // 
@@ -88,10 +87,9 @@ namespace SwineSyncc
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(390, 180);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(520, 222);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.Size = new System.Drawing.Size(114, 26);
             this.label3.TabIndex = 13;
             this.label3.Text = "Username";
             // 
@@ -99,10 +97,9 @@ namespace SwineSyncc
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(390, 272);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(520, 335);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.Size = new System.Drawing.Size(105, 26);
             this.label4.TabIndex = 14;
             this.label4.Text = "Password";
             // 
@@ -110,10 +107,9 @@ namespace SwineSyncc
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(373, 367);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(497, 452);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(669, 19);
+            this.label5.Size = new System.Drawing.Size(736, 23);
             this.label5.TabIndex = 15;
             this.label5.Text = "__________________________________________________________________";
             // 
@@ -122,10 +118,10 @@ namespace SwineSyncc
             this.eyeIcon.BackColor = System.Drawing.SystemColors.Window;
             this.eyeIcon.Cursor = System.Windows.Forms.Cursors.Hand;
             this.eyeIcon.Image = global::SwineSyncc.Properties.Resources.eye_closed;
-            this.eyeIcon.Location = new System.Drawing.Point(990, 296);
-            this.eyeIcon.Margin = new System.Windows.Forms.Padding(2);
+            this.eyeIcon.Location = new System.Drawing.Point(1320, 364);
+            this.eyeIcon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.eyeIcon.Name = "eyeIcon";
-            this.eyeIcon.Size = new System.Drawing.Size(26, 24);
+            this.eyeIcon.Size = new System.Drawing.Size(35, 30);
             this.eyeIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.eyeIcon.TabIndex = 23;
             this.eyeIcon.TabStop = false;
@@ -136,9 +132,10 @@ namespace SwineSyncc
             this.passwordTxt.BackColor = System.Drawing.SystemColors.Window;
             this.passwordTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.passwordTxt.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordTxt.Location = new System.Drawing.Point(394, 295);
+            this.passwordTxt.Location = new System.Drawing.Point(525, 363);
+            this.passwordTxt.Margin = new System.Windows.Forms.Padding(4);
             this.passwordTxt.Name = "passwordTxt";
-            this.passwordTxt.Size = new System.Drawing.Size(623, 27);
+            this.passwordTxt.Size = new System.Drawing.Size(830, 32);
             this.passwordTxt.TabIndex = 20;
             this.passwordTxt.UseSystemPasswordChar = true;
             // 
@@ -147,9 +144,10 @@ namespace SwineSyncc
             this.usernameTxt.BackColor = System.Drawing.SystemColors.Window;
             this.usernameTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.usernameTxt.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameTxt.Location = new System.Drawing.Point(393, 203);
+            this.usernameTxt.Location = new System.Drawing.Point(524, 250);
+            this.usernameTxt.Margin = new System.Windows.Forms.Padding(4);
             this.usernameTxt.Name = "usernameTxt";
-            this.usernameTxt.Size = new System.Drawing.Size(623, 27);
+            this.usernameTxt.Size = new System.Drawing.Size(830, 32);
             this.usernameTxt.TabIndex = 19;
             // 
             // logInBtn
@@ -161,9 +159,10 @@ namespace SwineSyncc
             this.logInBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.logInBtn.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.logInBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
-            this.logInBtn.Location = new System.Drawing.Point(393, 428);
+            this.logInBtn.Location = new System.Drawing.Point(524, 527);
+            this.logInBtn.Margin = new System.Windows.Forms.Padding(4);
             this.logInBtn.Name = "logInBtn";
-            this.logInBtn.Size = new System.Drawing.Size(622, 63);
+            this.logInBtn.Size = new System.Drawing.Size(829, 78);
             this.logInBtn.TabIndex = 18;
             this.logInBtn.Text = "LOGIN";
             this.logInBtn.UseVisualStyleBackColor = false;
@@ -174,27 +173,29 @@ namespace SwineSyncc
             this.roundedPictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.roundedPictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox1.BackgroundImage")));
             this.roundedPictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.roundedPictureBox1.Location = new System.Drawing.Point(75, 229);
+            this.roundedPictureBox1.Location = new System.Drawing.Point(100, 282);
+            this.roundedPictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.roundedPictureBox1.Name = "roundedPictureBox1";
-            this.roundedPictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.roundedPictureBox1.Size = new System.Drawing.Size(267, 246);
             this.roundedPictureBox1.TabIndex = 1;
             this.roundedPictureBox1.TabStop = false;
             // 
             // roundedPictureBox3
             // 
-            this.roundedPictureBox3.Location = new System.Drawing.Point(349, -2);
+            this.roundedPictureBox3.Location = new System.Drawing.Point(465, -2);
+            this.roundedPictureBox3.Margin = new System.Windows.Forms.Padding(4);
             this.roundedPictureBox3.Name = "roundedPictureBox3";
-            this.roundedPictureBox3.Size = new System.Drawing.Size(742, 684);
+            this.roundedPictureBox3.Size = new System.Drawing.Size(989, 842);
             this.roundedPictureBox3.TabIndex = 22;
             this.roundedPictureBox3.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1075, 681);
+            this.ClientSize = new System.Drawing.Size(1433, 838);
             this.Controls.Add(this.eyeIcon);
             this.Controls.Add(this.passwordTxt);
             this.Controls.Add(this.usernameTxt);
@@ -208,8 +209,8 @@ namespace SwineSyncc
             this.Controls.Add(this.label1);
             this.Controls.Add(this.roundedPictureBox3);
             this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
